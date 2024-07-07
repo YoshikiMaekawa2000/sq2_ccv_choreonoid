5d885a849bde56a0e523f8a95d8cd5aa56806114
YoshikiMaekawa2000 <yoshiki.maekawa@icloud.com>
2024-07-07 16:22:22 +0900

    caster joint

---
+++ script/calc_param_from_stl.py
@@ -0,0 +1,50 @@
+#calcurate inertia
+def calc_box_inertia(mass, x, y, z):
+    Ix = mass / 12.0 * (y**2 + z**2)
+    Iy = mass / 12.0 * (z**2 + x**2)
+    Iz = mass / 12.0 * (x**2 + y**2)
+    return Ix, Iy, Iz
+
+def calc_cylinder_inertia(mass, radius, length):
+    Ix = mass / 12.0 * (3.0 * radius**2 + length**2)
+    Iy = mass / 12.0 * (3.0 * radius**2 + length**2)
+    Iz = mass / 2.0 * radius**2
+    return Ix, Iy, Iz
+#main
+if __name__ == '__main__':
+    
+    ##calculate inertia of base_link
+    mass = 5.0
+    length = 0.105
+    width = 0.501
+    height = 0.044
+    Ix, Iy, Iz = calc_box_inertia(mass, length, width, height)
+    print("base_link:")
+    print(" Ix: ", Ix)
+    print(" Iy: ", Iy)
+    print(" Iz: ", Iz)
+
+    ##calculate inertia of steer_link
+    mass = 1.0
+    radius = 0.03
+    length = 0.15
+    Ix, Iy, Iz = calc_cylinder_inertia(mass, radius, length)
+    print("steer_link:")
+    print(" Ix: ", Ix)
+    print(" Iy: ", Iy)
+    print(" Iz: ", Iz)
+
+    ##calculate inertia of wheel_link
+    mass = 5.0
+    radius = 0.1435
+    length = 0.025
+    Ix, Iy, Iz = calc_cylinder_inertia(mass, radius, length)
+    print("wheel_link:")
+    print(" Ix: ", Ix)
+    print(" Iy: ", Iy)
+    print(" Iz: ", Iz)
+
+
+  
+
+
